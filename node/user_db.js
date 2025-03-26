34cf8a6333004d142f555c0af973c8008e40a319
adria <adrianady623@gmail.com>
2025-03-26 20:07:25 +0200

    ceva db

--- node/user_db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { Pool} = require('pg');
-
-const poolUser = new Pool({
-    connectionString: String(process.env.DATABASE_URL),
-});
-module.exports = poolUser;
-

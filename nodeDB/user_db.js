0de0a6af800d38aa54b5cccca58d2455269a3b5a
adria <adrianady623@gmail.com>
2025-03-26 23:09:20 +0200

    SIGNUP functional

--- nodeDB/user_db.js
+++ nodeDB/user_db.js
@@ -1,8 +1,15 @@
-require('dotenv').config();
-const { Pool} = require('pg');
+require('dotenv').config({path: '../.env'});
 
+
+const { Pool } = require('pg'); // Import Pool from pg instead of Client
+
+// Set up the Pool with environment variables
 const pool = new Pool({
-    connectionString:process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, // Changed from `username` to `user` for Pool configuration
+    password: String(process.env.DB_PASSWORD),
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
-module.exports.userPool = pool;
 
+module.exports = { pool }; // Export pool instead of client
